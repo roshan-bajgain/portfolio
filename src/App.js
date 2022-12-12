b0faf6110e4e817761d1f3aecf7ebedf6e020cfb
unknown <roshan.bazgain@gmail.com>
2022-12-12 15:59:10 +0545

    deleted all unnecessary file

--- src/App.js
+++ src/App.js
@@ -1,27 +1,8 @@
-import './App.css';
-import Header from './components/header/Header';
-import Nav from './components/nav/Nav';
-import About from './components/about/About';
-import Experience from './components/experience/Experience';
-import Service from './components/service/Service';
-import Portfolio from './components/portfolio/Portfolio';
-import Testimonial from './components/testimonial/Testimonial';
-import Contact from './components/contact/Contact';
-import Footer from './components/footer/Footer';
+import React from 'react';
 
 function App() {
   return (
-    <>
-      <Header/>
-      <Nav />
-      <About />
-      <Experience />
-      <Service />
-      <Portfolio />
-      <Testimonial />
-      <Contact />
-      <Footer />
-    </>
+    <div>Hello fucker</div>
   );
 }
 
