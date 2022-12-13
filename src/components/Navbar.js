0209ecf6610a5571ba1629b9aebd6527ba553da3
unknown <roshan.bazgain@gmail.com>
2022-12-13 16:51:39 +0545

    Add styling for mobile verson

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,27 @@
 import './Navbar.css';
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-const Navbar = () => (
+import {FaBars, FaTimes} from 'react-icons/fa'
+
+const Navbar = () => {
+const[click,setclick] = useState(false);
+const handleclick = () => setclick(!click) //not clicked
+return(
   <div className="header">
     <Link to="/">
       <h1>Portfolio</h1>
     </Link>
-    <ul className='nav-menu'>
+    <ul className="nav-menu">
       <li><Link to="/">Home</Link></li>
       <li><Link to="/project">Project</Link></li>
       <li><Link to="/about">About</Link></li>
       <li><Link to="/contact">Contact</Link></li>
     </ul>
+    <div className="havburger">
+      {click ? ( <FaTimes size={20} style={{color:"#fff"}} />):(<FaBars size={20} style={{color:"#fff"}} />)}
+    </div>
   </div>
-);
+)};
 
 export default Navbar;
