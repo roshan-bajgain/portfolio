352155ae5af585e2487567cf0011ba7545cd3c9f
unknown <roshan.bazgain@gmail.com>
2022-12-13 14:56:01 +0545

    Add navlink

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './routes/Home';
+import About from './routes/About';
+import Contact from './routes/Contact';
+import Project from './routes/project';
 
 function App() {
   return (
-    <div>Hello fucker</div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/routes/project.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Project = () => (
+  <div>project</div>
+);
+
+export default Project;
