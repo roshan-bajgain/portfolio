274c71694582bcb32e2c23c98f19b7d05f3ee62a
unknown <roshan.bazgain@gmail.com>
2022-12-28 14:06:59 +0545

    Add project-cards

---
+++ src/components/WorkCard.js
@@ -0,0 +1,29 @@
+import './WorkCardstyle.css';
+
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+
+const WorkCard = () => {
+  return (
+    <div className="work-container">
+        <h1 className="project-heading">
+            Projects
+        </h1>
+        <div className="project-container">
+            <div className="project-card">
+                <img src="" alt="image" />
+                <h2 className="project-title">Project Title</h2>
+                <div className="pro-details">
+                    <p>This is my project</p>
+                    <div className="pro-btns">
+                        <NavLink to="url.com" className="btn">Live-demo</NavLink>
+                        <NavLink to="url.com" className="btn">Source</NavLink>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default WorkCard;
--- src/routes/project.js
+++ src/routes/project.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import HeroImg2 from '../components/HeroImg2';
+import WorkCard from '../components/WorkCard';
 
 const Project = () => (
   <div>
     <Navbar />
     <HeroImg2 heading = "PROJECTS" text = "Some of my recent works"/>
+    <WorkCard />
     <Footer />
   </div>
 );
