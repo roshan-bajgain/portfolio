6eba3d4957cedc46cb30483e3b7e1b8a216909d0
unknown <roshan.bazgain@gmail.com>
2022-12-31 12:36:00 +0545

    Fix linter error

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -11,16 +11,16 @@ const AboutContent = () => (
       <h1>Who am I?</h1>
       <p>I am a full-stack developer. I create responsive secure website for my clients</p>
       <Link to="/contact">
-        <button className="btn">Contact</button>
+        <button className="btn" type="submit">Contact</button>
       </Link>
     </div>
     <div className="right">
       <div className="img-container">
         <div className="img-stack top">
-          <img src={react1} className="img" alt="React image" />
+          <img src={react1} className="img" alt="React" />
         </div>
         <div className="img-stack bottom">
-          <img src={Rails} className="img" alt="Rails image" />
+          <img src={Rails} className="img" alt="Rails" />
         </div>
 
       </div>
