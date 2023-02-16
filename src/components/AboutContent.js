9e305a179f7ea58a1bdc7f5d5e89671274c12e78
roshan-bajgain <roshan.bazgain@gmail.com>
2023-02-16 11:45:14 +0545

    Add skills

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -26,14 +26,48 @@ const AboutContent = () => (
       </div>
     </div>
   </div>
-  {/* <div className="skills>">
-    <h2 className="language">Language</h2>
-    <ul className="list">
-      <li className="lang">HTML</li>
-      <li>CSS</li>
-      <li>JavaScript</li>
-    </ul>
-  </div> */}
+  <h2 className="head">SKILLS</h2>
+  <hr></hr>
+  <div className="container">
+    <div className="card">
+      <div className="box">
+        <div className="content">
+          <h2>Language</h2>
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>Ruby</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div className="card">
+      <div className="box">
+        <div className="content">
+          <h2>Framework</h2>
+          <ul>
+            <li>React</li>
+            <li>Redux</li>
+            <li>Ruby on Rails</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div className="card">
+      <div className="box">
+        <div className="content">
+          <h2>Tools & Methods</h2>
+          <ul>
+            <li>Git</li>
+            <li>GitHub</li>
+            <li>Remote Pair-Programming</li>
+            <li>Teamwork</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
   </>
   
 );
